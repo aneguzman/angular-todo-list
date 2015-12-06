493351da5805439d08a31566aaaad90332e37e05
Aneudy Guzman <aneguzman@gmail.com>
2015-12-06 17:49:48 -0400

    Refactoring code

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+﻿var model = {
+    user: "Aneudy"
+};
+
+var todoApp = angular.module("todoApp", []);
+
+todoApp.run(function ($http) {
+    $http.get('todo.json').success(function (data) {
+        model.items = data;
+    });
+});
+
+todoApp.filter("checkedItems", function () {
+    return function (items, showComplete) {
+        var resultArr = [];
+        angular.forEach(items, function (item) {
+            if (item.done == false || showComplete == true) resultArr.push(item);
+        });
+        return resultArr;
+    };
+});
+
+todoApp.controller("ToDoCtrl", function ($scope) {
+    $scope.todo = model;
+
+    $scope.incompleteCount = function () {
+        var count = 0;
+        angular.forEach($scope.todo.items, function (item) {
+            if (!item.done) count += 1;
+        });
+        return count;
+    };
+
+    $scope.warningLevel = function () {
+        return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
+    };
+
+    $scope.addNewItem = function (actionText) {
+        $scope.todo.items.push({ action: actionText, done: false });
+    };
+});
